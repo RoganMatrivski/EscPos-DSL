fa3a8019e016d700a144c34d7f553974dc592bee
Robin Mauritz <robin@mdev.co.id>
2026-08-04 15:08:47 +0700

    Init commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use boltffi::*;
+
+#[data]
+#[derive(Clone, Copy)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+#[export]
+pub fn distance(a: Point, b: Point) -> f64 {
+    ((b.x - a.x).powi(2) + (b.y - a.y).powi(2)).sqrt()
+}
